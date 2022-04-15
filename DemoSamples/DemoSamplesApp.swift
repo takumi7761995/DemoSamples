7511678c6c8063d3db5d2292264189eb31f0e7f1
杉本匠 <takumi1995776@gmail.com>
2022-04-16 02:02:05 +0900

    Initial Commit

---
+++ DemoSamples/DemoSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSamplesApp.swift
+//  DemoSamples
+//
+//  Created by 杉本匠 on 2022/04/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
